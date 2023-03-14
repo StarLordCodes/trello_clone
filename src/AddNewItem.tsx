5beb11a86866458e386678770dffd85b22a1ea33
Abhijith Mammoottil <amammoottil@outlook.com>
2023-03-14 22:14:45 +0530

    deleted comments from AddNewItem and NewItemForm, also added NewItemForm to AddNewItem

--- src/AddNewItem.tsx
+++ src/AddNewItem.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { AddItemButton } from "./styles"
+import { NewItemForm } from "./NewItemForm";
 
 type AddNewItemProps = {
     onAdd(text:string): void
@@ -11,14 +12,17 @@ export const AddNewItem = (props: AddNewItemProps) => {
     const [showForm, setShowForm] = useState(false);
     const { onAdd, toggleButtonText, dark } = props;
     if (showForm) {
-        // We show item creation form here
+        <NewItemForm
+        onAdd={text => { 
+            onAdd(text)
+            setShowForm(false)}
+        }/>
     }
     return (
         <AddItemButton dark={dark} onClick={()=> setShowForm(true)}>
             {toggleButtonText}
         </AddItemButton>
     )
-    // AddNewItem holds a showForm boolean state, when this state is true
-    // we show an input with the "Create" button , when it's false, we
-    // render the button with toggleButtonText on it.
+    // our AddNewItem component is now fully functional and we can add it to the 
+    // application layout.
 }
--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -11,14 +11,10 @@ export const NewItemForm = ({ onAdd }: NewItemFormProps) =>{
     return (
         <NewItemFormContainer>
             <NewItemInput value={text} onChange={e=>setText(e.target.value)}/>
-            {/*this component uses a controlled input, we store the value for it in
-             the text state. Whenever you type inside this iput, the text state is updated */}
             <NewItemButton onClick={() => onAdd(text)}>
                 Create
             </NewItemButton>
         </NewItemFormContainer>
     )
-    // Here we didn't have to provide any type for the event argument of our onChange callback.
-    // Typescript gets the typefrom React type definitions
    
 }
